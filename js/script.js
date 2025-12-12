ff5485438c55783e35df6248ad5f79476944c8a1
sudhigirijaa <sudhi.bhakta@gmail.com>
2025-12-12 13:58:22 -0500

    Delete js/script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector("nav ul");
-
-hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-});
